92c0d50413dc06cebdf90fba41fd96fc8798c908
Salma Khan <74590990+salmakhan0@users.noreply.github.com>
2022-11-21 00:31:53 +0600

    Add files via upload

---
+++ bmi 01.js
@@ -0,0 +1,10 @@
+const firstName = "Fahima";
+const job = "student";
+const bday = 1994;
+const year = 2022;
+
+const senTence1 = "I'm " + firstName + ', a ' + (year-bday) + ' year old ' + job + '!';
+
+const senTence = ` I'm ${firstName}, a ${year-bday} year old ${job} ! ` ;
+
+console.log(senTence);
